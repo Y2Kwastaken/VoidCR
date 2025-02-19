cd21fe6bd107e39bd2ef0a9649a5a568f8fbb3a8
Y2Kwastaken <mwholder@proton.me>
2025-02-18 19:59:46 -0600

    Fixup API Method Defaults

--- buildSrc/src/main/kotlin/sh/miles/voidcr/artisan/APIMethodExtension.kt
+++ buildSrc/src/main/kotlin/sh/miles/voidcr/artisan/APIMethodExtension.kt
@@ -3,6 +3,8 @@ package sh.miles.voidcr.artisan
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.Opcodes.ACC_PRIVATE
 import org.objectweb.asm.Opcodes.ACC_PUBLIC
+import org.objectweb.asm.Opcodes.ACC_TRANSIENT
+import org.objectweb.asm.Opcodes.ACC_TRANSITIVE
 import org.objectweb.asm.Opcodes.ALOAD
 import org.objectweb.asm.Opcodes.ARETURN
 import org.objectweb.asm.Opcodes.ASM9
@@ -60,16 +62,17 @@ class ApiMethodContainerHandler : ContainerHandler {
     override fun parse(literal: LiteralResult, logger: ArtisanLogger) {
         val pair = literal.literal.split(" ").map { JvmClasspath(JvmClasspath.CLASS, it, null, null) }
         generations[pair[1]] = pair[0]
-        val expandAccess = literal.getMetaValue("ExtendAccess")
-        if (expandAccess != null) extendAccess[pair[0]] = ArtisanAccessUtil.scopeToOpcode(expandAccess)
+        val expandAccess = literal.getMetaValue("DefineAccess")
+        if (expandAccess != null) extendAccess[pair[0]] =
+            expandAccess.split(",").sumOf { ArtisanAccessUtil.scopeToOpcode(it) }
         logger.info("Found Api Generation for class ${pair[0].path} in ${pair[1].path}")
     }
 
     override fun visit(node: ClassNode, path: JvmClasspath, logger: ArtisanLogger) {
         val generation = this.generations[path]!!
-        val expandAccess = this.extendAccess.getOrDefault(generation, 0)
+        val expandAccess = this.extendAccess.getOrDefault(generation, ACC_PRIVATE)
         node.fields.add(
-            FieldNode(ASM9, ACC_PRIVATE + expandAccess, "cache", "L${generation.path};", null, null)
+            FieldNode(ASM9, ACC_TRANSIENT + expandAccess, "cache", "L${generation.path};", null, null)
         )
 
 
